a49c5541bc8a83de16c7fbd9ee267c334f5f2ac5
Mauricio Cordeiro <mauriciocordeiro@live.com>
2021-02-08 19:02:37 -0300

    fixing docker frontend

--- service-front/src/assets/env.js
+++ service-front/src/assets/env.js
@@ -2,6 +2,6 @@
     window["env"] = window["env"] || {};
 
     window["env"]["urlAuth"] = "http://auth:5002/";
-    window["env"]["urlCrud"] = "http://crug:5001/";
+    window["env"]["urlCrud"] = "http://crud:5001/";
     window["env"]["urlLog"] = "http://log:5003/";
 })(this);
--- service-front/src/assets/env.template.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-    window.env = window.env || {};
-
-    // Environment variables
-    window["env"]["urlAuth"] = "${urlAuth}";
-    window["env"]["urlCrud"] = "${urlCrud}";
-    window["env"]["urlLog"] = "${urlLog}";
-})(this);
